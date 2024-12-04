4215773714cbb779e1ca32c60398236e0e4afdcf
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-12-04 00:36:37 -0500

    avances

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,159 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.pb-0.pb-md-0
+    .titulo-principal.color-acento-contenido.mb-0(data-aos="flip-up")
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Modulación
+    
+    .fondo-personalizado-1.bottom-2.f1.pt-4
+      .fondo-personalizado-1__contenido
+        .row.align-items-center.mb-4
+          .col-lg-8(data-aos="fade-right")
+            p.mb-0 Modificación sistemática, paso a paso, de una onda portadora de acuerdo con el mensaje o señal modulada. Se utiliza cuando se transmite la señal analógica a una frecuencia diferente o con un ancho de banda menor o mayor. La modulación se puede efectuar con los cambios de amplitud, frecuencia o fase de la señal portadora.
+            .titulo-decorativo.mt-5
+              .cont-figura
+                img(src="@/assets/curso/temas/img-tit-dec.svg")
+              .cont-text 
+                h3 Características
+          .col-lg-4.order-first.order-lg-last(data-aos="fade-left")
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/26.svg",style="max-width: 400px;")
+        
+        p.mb-4(data-aos="fade-up") A continuación, se presentan las principales características de la modulación:
+
+        .row.justify-content-center
+          .col-lg-10(data-aos="fade-right")
+            .fondo-color-sistema-m.borde-12.p-4.p-lg-5
+              .row.align-items-center
+                .col-lg-4
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/27.png",style="max-width: 236px;")
+                .col-lg-8
+                  ul.lista-ul.mb-0.pb-0
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Maximiza el ancho de banda de cada canal.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Permite la propagación de la señal de información por cable o por el aire.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Influye en la calidad de la información transmitida.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Disminuye el tamaño de las antenas.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Minimiza interferencia entre canales.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Ordena el radioespectro al seleccionar canales para cada información.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Protege la información de las afectaciones por ruido.
+    
+        .row.mb-4(data-aos="fade-right")
+          .col-lg-7.mb-4.mb-lg-0
+            .titulo-decorativo.mt-4.mt-lg-2.mb-4
+              .cont-figura
+                img(src="@/assets/curso/temas/img-tit-dec.svg")
+              .cont-text 
+                h3 Tipos de modulación
+            
+            p.mb-0 Existen diferentes tipos de modulación que se utilizan en comunicaciones analógicas:
+          .col-lg-5.position-relative
+            figure.figura-dec-1
+              img.mx-auto(src="@/assets/curso/temas/28.svg",style="max-width: 505px;")
+    
+        .fondo-color-sistema-p.p-4.mb-2
+          .row
+            .col-lg-6(data-aos="fade-right")
+              .fondo-color-sistema-l.d-inline-flex.borde-20.py-1.px-4.mb-4
+                h3.mb-0 Amplitud Modulada (AM)
+              p.mb-0 Este tipo de modulación modifica la amplitud de la onda portadora, de manera que esta varía según los cambios en el nivel de la señal moduladora, aunque la frecuencia de la portadora permanece constante.
+            .col-lg-6(data-aos="fade-left")
+              figure.mb-4.mb-lg-0.pt-4
+                img.me-auto(src="@/assets/curso/temas/29.svg",style="max-width: 505px;")
+    .fondo-color-sistema-p.p-4.mb-5(data-aos="fade-down")
+      .row
+        .col-lg-6
+          .fondo-color-sistema-l.d-inline-flex.borde-20.py-1.px-4.mb-4
+            h3.mb-0 Frecuencia Modulada (FM)
+          p.mb-0 En esta modulación, se establece un nivel mínimo de la señal de RF que el receptor puede detectar, permitiendo obtener una señal clara y nítida de la información transmitida.
+        .col-lg-6
+          figure.mb-4.mb-lg-0.pt-4
+            img.me-auto(src="@/assets/curso/temas/30.svg",style="max-width: 505px;")
+    
+    p.mb-4(data-aos="fade-left") En la figura 2 se diferencia el comportamiento de la señal portadora con la señal modulada en amplitud y la señal modulada en frecuencia.
+
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+          p.mb-0 #[b Figura 2.] Modulación AM y FM
+
+        .fondo-color-sistema-q.p-4(data-aos="flip-up")
+          figure.d-none.d-lg-block
+            img.mx-auto(src="@/assets/curso/temas/31.svg",style="max-width: 687px;",alt="Figura que presenta ejemplos de ondas en distintos estados de modulación. La primera es una onda portadora, seguida por una onda moduladora. Luego, se observa una onda modulada FM, donde la frecuencia varía. Finalmente, aparece una onda modulada AM, en la cual la amplitud cambia según la señal moduladora.")
+          figure.d-lg-none
+            img.mx-auto(src="@/assets/curso/temas/32.svg",style="max-width: 500px;",alt="Figura que presenta ejemplos de ondas en distintos estados de modulación. La primera es una onda portadora, seguida por una onda moduladora. Luego, se observa una onda modulada FM, donde la frecuencia varía. Finalmente, aparece una onda modulada AM, en la cual la amplitud cambia según la señal moduladora.")
+    
+    .fondo-color-sistema-l.d-inline-flex.borde-20.py-1.px-4.mb-4(data-aos="fade-left")
+      h3.mb-0 Aplicaciones en AM y FM
+    
+    p.mb-2(data-aos="fade-right") Las aplicaciones en las ondas moduladas en amplitud (AM) son:
+
+    .fondo-personalizado-1.f1
+      .fondo-personalizado-1__contenido.pt-3.pb-5
+        .row.justify-content-center.mb-4
+          .col-lg-10(data-aos="fade-right")
+            .fondo-color-sistema-m.borde-12.p-4.p-lg-5
+              .row
+                .col-lg-4
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/33.png",style="max-width: 236px;")
+                .col-lg-8
+                  ul.lista-ul.mb-0.pb-0
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Ondas cortas y medias.
+                    li.d-flex.mb-2
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span VHF 550 a 1.600 kHz usadas en comunicaciones radiales entre los aviones y las torres de control de los aeropuertos.
+                    li.d-flex.mb-0
+                      i.fas.fa-check-circle.c-color-sistema-n
+                      span Radiofonía.
+        
+        p.mb-4(data-aos="fade") Las aplicaciones para el caso de las ondas moduladas en frecuencia (FM) son:
+
+        .row.justify-content-center.mb-3
+          .col-lg-2.col-md-6.col-sm-9.mb-4(data-aos="fade-right")
+            .borde-12.fondo-color-sistema-q.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/34.png")
+              p.mb-0 La difusión del sonido de la televisión analógica.
+          .col-lg-2.col-md-6.col-sm-9.mb-4(data-aos="fade")
+            .borde-12.fondo-color-sistema-q.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/35.png")
+              p.mb-0 Enviar señales al espacio.
+          .col-lg-2.col-md-6.col-sm-9.mb-4(data-aos="fade")
+            .borde-12.fondo-color-sistema-q.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/36.png")
+              p.mb-0 Radiofrecuencias de muy alta frecuencia.
+          .col-lg-2.col-md-6.col-sm-9.mb-4(data-aos="fade")
+            .borde-12.fondo-color-sistema-q.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/37.png")
+              p.mb-0 La grabación de video.
+          .col-lg-2.col-md-6.col-sm-9.mb-4(data-aos="fade-left")
+            .borde-12.fondo-color-sistema-q.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/38.png")
+              p.mb-0 Recuperación de la cinta magnética sin distorsión extrema.
+               
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,10 +2,149 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Filtros y receptores de radiofrecuencia
+    
+    p.mb-0(data-aos="fade-right") Dispositivo que modifica una señal analógica de entrada en función de la frecuencia. Utilizan elementos pasivos en sus circuitos electrónicos: resistencias, condensadores, inductores, amplificadores operacionales, entre otros. Son empleados para operaciones de reducción de ruido, ecualizadores gráficos y mejoramiento de señales de vídeo.
+
+    .titulo-decorativo.my-4.mb-4(data-aos="fade-up")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Tipos de filtros
+    
+    p.mb-4(data-aos="fade-left") A continuación, se describen los principales tipos de filtros utilizados en sistemas de comunicaciones:
+
+    .fondo-personalizado-1.f2(data-aos="fade-up")
+      .fondo-personalizado-1__contenido.py-5
+        .bg-white.p-4.borde-20
+          SlyderA.slider-personalizado-1.pt-lg-3(tipo="b")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Filtro pasa bajos
+                p.mb-0 Este filtro permite el paso de frecuencias bajas con muy poca atenuación, conocidas como frecuencia de corte. Las frecuencias que exceden este límite son significativamente reducidas.
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/40.svg",style="max-width: 247px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Filtro pasa altos
+                p.mb-0 Atenúa mínimamente las frecuencias superiores a la frecuencia de corte, mientras que aplica una mayor atenuación a las frecuencias inferiores a este límite.
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/41.svg",style="max-width: 247px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Filtro pasa banda
+                p.mb-0 Este filtro cuenta con dos frecuencias de corte, una inferior y otra superior. Permite el paso de señales dentro de un rango específico, bloqueando las frecuencias fuera de ese rango.
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/42.svg",style="max-width: 247px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Filtro rechaza banda
+                p.mb-0 Elimina todas las señales cuyas frecuencias se encuentren dentro del rango definido entre la frecuencia de corte inferior y la frecuencia de corte superior.
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/43.svg",style="max-width: 247px;")
+    
+    .titulo-decorativo.my-4(data-aos="fade-up")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Aplicaciones de los filtros
+    
+    p.mb-4(data-aos="fade") Los filtros tienen diversas aplicaciones en sistemas de comunicaciones, cada uno diseñado para cumplir funciones específicas en el procesamiento de señales:
+
+    .row
+      .col-lg-3(data-aos="fade-right")
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/44.png",style="max-width: 295px;")
+      .col-lg-9(data-aos="fade-left")
+        AcordionA.acordion-personalizado-1.mb-0(tipo="a" clase-tarjeta="tarjeta")
+          .div(titulo="Filtros <em>antialiasing</em>")
+            .ps-md-5.ms-md-3
+              p Empleados en los sistemas de señal mixta: sistemas de conversión A/D (Analógico-Digital) y D/A (Digital-Analógico).
+          .div(titulo="Filtros de reconstrucción")
+            .ps-md-5.ms-md-3
+              p Utilizados para regenerar señales.
+          .div(titulo="Ecualizadores")
+            .ps-md-5.ms-md-3
+              p Adecuan una señal a frecuencias específicas.
+          .div(titulo="Amplificadores de señales analógicas")
+            .ps-md-5.ms-md-3
+              p Funcionan como circuitos selectivos para amplificar las señales a las frecuencias específicas y mitigar las señales fuera de banda.
+          .div(titulo="Procesamiento de señales")
+            .ps-md-5.ms-md-3
+              p Procesa e interpreta las señales.
+          .div(titulo="Expulsor de ruido")
+            .ps-md-5.ms-md-3
+              p Elimina el ruido que aparece junto a una señal, siempre que la frecuencia sea conocida o específica.
+    
+    .titulo-decorativo.my-4(data-aos="fade")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Receptores de radiofrecuencia
+    
+    .row.mb-4
+      .col-lg-9(data-aos="fade-right")
+        p.mb-4 Dispositivos que demodulan una señal analógica de radiofrecuencia para obtener información o la señal original enviada por el sistema de comunicaciones. La señal de radiofrecuencia se presenta en onda continua o modulada en amplitud (AM) y en onda modulada en frecuencia (FM). Por esta razón, existen diferentes receptores de radiofrecuencia clasificados de acuerdo con el tipo de señal a recibir y según el rango de frecuencia en el que deben operar.
+
+        .fondo-color-sistema-q.p-4
+          p.mb-0 Si la señal de origen en el receptor presenta una amplitud baja, la función del receptor es amplificar la señal de entrada por un factor de algunos miles, para recuperar la amplitud de la señal original.
+      .col-lg-3.order-first.order-lg-last(data-aos="fade-left")
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/45.svg",style="max-width: 295px;")
+    
+    .fondo-personalizado-1.top-1.bottom-1.f1
+      .fondo-personalizado-1__contenido
+        .row(data-aos="flip-up")
+          .col-lg-4
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/46.svg",style="max-width: 400px;")
+          .col-lg-8
+            .fondo-color-sistema-r.p-4.h-100.align-content-center
+              p.mb-0 La señal de radiofrecuencia se presenta en onda continua o modulada en amplitud (AM) y en onda modulada en frecuencia (FM). Por esta razón, existen diferentes receptores de radiofrecuencia clasificados de acuerdo con el tipo de señal a recibir y según el rango de frecuencia en el que deben operar.
+        
+        .titulo-decorativo.my-5(data-aos="fade-down")
+          .cont-figura
+            img(src="@/assets/curso/temas/img-tit-dec.svg")
+          .cont-text 
+            h3 Amplificador de baja e intermedia frecuencia
+        
+        p.mb-4(data-aos="fade") Ejerce el filtrado y la amplificación a frecuencia intermedia que es la frecuencia menor a la de la señal de entrada, producto de la suma de la señal de recepción y la generada en el oscilador local (circuito electrónico). Este fenómeno permite mayor estabilidad y sensibilidad en las señales.
+
+        .row.justify-content-center(data-aos="flip-up")
+          .col-lg-10
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 3.] Diagrama receptor radiofrecuencia sintonizada
+            .fondo-color-sistema-k.p-4
+              figure
+                img.mx-auto(src="@/assets/curso/temas/47.svg",style="max-width: 792px;",alt="Diagrama de un sistema de recepción de radiofrecuencia. La señal comienza en la antena, pasa al circuito de entrada, luego al amplificador de RF, sigue al detector, pasa por el amplificador de AF y finalmente llega a la carga. Se muestran gráficos de una onda de RF (radiofrecuencia) y una onda de AF (audiofrecuencia) en función de la amplitud y el tiempo.")
+    
+    .titulo-decorativo.my-4(data-aos="fade-left")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Doble conversión
+    
+    p.mb-4(data-aos="fade-right") Este tipo de receptores se identifican porque efectúan dos combinaciones con el objetivo de lograr la señal de frecuencia intermedia deseada.
+
+    .titulo-decorativo.my-4(data-aos="fade-left")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Conversión voltaje a frecuencia y viceversa
+    
+    p.mb-0(data-aos="fade-right") Circuitos integrados que convierten un voltaje de entrada de una señal análoga en un tren de pulsos cuya frecuencia de salida es similar al nivel de entrada, es decir, su función es la de convertir una señal analógica a una serie de pulsos.
+
 
 </template>
 
